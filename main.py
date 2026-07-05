9c083fa0cc9ce12647de4e9eb4545db775555047
manthanjindal <manthanjindal08@gmail.com>
2026-07-05 12:23:08 +0530

    improved ux

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ menu = {"pizza":3.00,"nachos":4.50,"popcorn": 6.00,"fries": 2.50,"chips": 1.00,"
 cart = []
 total = 0
 
-for key in menu.keys():
-    print(f"{key} : {menu[key]}")
+for key,value in menu.items():
+    print(f"{key:10} : {value:.2f}")
 
 print("What would you like to order")
 while True:
     item = input()
     print("is there anything else you'd like to order?")
-    print("(press '67' to exit)")
-    if item == '67':
-        print(f"thank you for shopping at the mj consession store\nYour total is {total}")
+    if 'no' in item or 'na' in item:
+        print(f"\nthank you for shopping at the mj consession store\n\n-------------------------------\n\tYour total is ${total}/- \n-------------------------------\n")
         break
     if item not in menu.keys():
         print("we dont serve that")
